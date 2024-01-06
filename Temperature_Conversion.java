1376b364b5f0275ea9b7e7eff4d04dd834058749
NIHAL SHANKAR KARANAM <120080322+22MH1A05I3@users.noreply.github.com>
2024-01-06 12:02:51 +0530

    import java.util.*; public class ans{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int c=sc.nextInt();         float f=(float) (32+(c*9.0/5.0));         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ans{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        float f=(float) (32+(c*9.0/5.0));
+        System.out.printf("%.2f",f);
+    } 
+}
